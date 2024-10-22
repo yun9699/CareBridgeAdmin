80a4387df541b06359ea95ac7adcc84304bf01f8
DSL0680 <ekthf988172@gmail.com>
2024-10-22 09:35:39 +0900

    feat : admin detail router - add (임시)

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,23 +1,31 @@
 import {createBrowserRouter} from "react-router-dom";
-import MainPage from "../page/MainPage.jsx";
 import AdminIndex from "../page/AdminIndex.jsx";
 import FAQRouter from "./FAQRouter.jsx";
 import QNARouter from "./QNARouter.jsx";
 import CareGiverRouter from "./CareGiverRouter.jsx";
 import CareTakerRouter from "./CareTakerRouter.jsx";
+import {lazy} from "react";
+
+const CareTakerDetail = lazy(() => import("../page/AdminDetailPage.jsx"))
 
 const MainRouter = createBrowserRouter([
     {
         path: '/',
         element: <AdminIndex/>,
         children: [
+            CareTakerRouter,
 
-        ]
+            {
+                path: "ctDetail",
+                element: <CareTakerDetail/>,
+            }
+        ],
+
     },
     FAQRouter,
     QNARouter,
-    CareGiverRouter,
-    CareTakerRouter
+    CareGiverRouter
+
 
 
     ]);
