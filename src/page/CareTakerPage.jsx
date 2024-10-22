e88fdaaff09e9fdd40b56f65f0d61c538a31e81b
DSL0680 <ekthf988172@gmail.com>
2024-10-22 09:36:00 +0900

    feat : careTaker (임시)

--- src/page/CareTakerPage.jsx
+++ src/page/CareTakerPage.jsx
@@ -1,10 +1,15 @@
 import CareTakerComponent from "../component/caretaker/CareTakerComponent.jsx";
 
+import {Outlet} from "react-router-dom";
+
 function CareTakerPage() {
     return (
 
+        <div>
         <CareTakerComponent/>
 
+        <Outlet/>
+        </div>
     );
 }
 
