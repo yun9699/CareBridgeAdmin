912b8237c365c0a6a30fbe275b4439b98e6eabe2
Dongeon Lee <duqdjs123@naver.com>
2024-10-23 15:41:53 +0900

    feat: windmill no

--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -5,38 +5,245 @@ function BasicLayout({ children }) {
 
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
+    const [isPagesMenuOpen, setIsPagesMenuOpen] = useState(false);
+
     return (
         <div className="flex h-screen bg-green-50">
             {/* 사이드바 */}
-            <aside
-                className={`bg-green-800 text-green-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} md:relative md:translate-x-0 transition duration-200 ease-in-out`}>
-                <nav>
-                    <a href="#"
-                       className="block py-2.5 px-4 rounded transition duration-200 hover:bg-green-700 hover:text-white">
-                        <Home className="inline-block mr-2" size={20}/>
-                        대시보드
+            <aside className="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0">
+                <div className="py-4 text-gray-500 dark:text-gray-400">
+                    <a
+                        className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
+                        href="#"
+                    >
+                        Windmill
                     </a>
-                    <a href="#"
-                       className="block py-2.5 px-4 rounded transition duration-200 hover:bg-green-700 hover:text-white">
-                        <Users className="inline-block mr-2" size={20}/>
-                        사용자 관리
-                    </a>
-                    <a href="#"
-                       className="block py-2.5 px-4 rounded transition duration-200 hover:bg-green-700 hover:text-white">
-                        <DollarSign className="inline-block mr-2" size={20}/>
-                        간병비 관리
-                    </a>
-                    <a href="#"
-                       className="block py-2.5 px-4 rounded transition duration-200 hover:bg-green-700 hover:text-white">
-                        <FileText className="inline-block mr-2" size={20}/>
-                        서비스 기록
-                    </a>
-                    <a href="#"
-                       className="block py-2.5 px-4 rounded transition duration-200 hover:bg-green-700 hover:text-white">
-                        <Settings className="inline-block mr-2" size={20}/>
-                        설정
-                    </a>
-                </nav>
+                    <ul className="mt-6">
+                        <li className="relative px-6 py-3">
+            <span
+                className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"
+            ></span>
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
+                                href="index.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                                </svg>
+                                <span className="ml-4">Dashboard</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className="relative px-6 py-3">
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                href="forms.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
+                                </svg>
+                                <span className="ml-4">Forms</span>
+                            </a>
+                        </li>
+                        <li className="relative px-6 py-3">
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                href="cards.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>
+                                </svg>
+                                <span className="ml-4">Cards</span>
+                            </a>
+                        </li>
+                        <li className="relative px-6 py-3">
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                href="charts.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"/>
+                                    <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"/>
+                                </svg>
+                                <span className="ml-4">Charts</span>
+                            </a>
+                        </li>
+                        <li className="relative px-6 py-3">
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                href="buttons.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"/>
+                                </svg>
+                                <span className="ml-4">Buttons</span>
+                            </a>
+                        </li>
+                        <li className="relative px-6 py-3">
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                href="modals.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"/>
+                                </svg>
+                                <span className="ml-4">Modals</span>
+                            </a>
+                        </li>
+                        <li className="relative px-6 py-3">
+                            <a
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                href="tables.html"
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
+                                </svg>
+                                <span className="ml-4">Tables</span>
+                            </a>
+                        </li>
+                        <li className="relative px-6 py-3">
+                            <button
+                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+
+                            >
+                                <svg
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path d="M3 12h18M3 6h18M3 18h18"/>
+                                </svg>
+                                <span className="ml-4">Pages</span>
+                                <svg
+                                    className={`w-4 h-4 ml-auto transition-transform duration-150 ${isPagesMenuOpen ? 'transform rotate-180' : ''
+                                    }`}
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M19 9l-7 7-7-7"
+                                    />
+                                </svg>
+                            </button>
+                            {isPagesMenuOpen && (
+                                <ul className="mt-2">
+                                    <li className="relative px-6 py-3">
+                                        <a
+                                            className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                            href="404.html"
+                                        >
+                                            <span className="ml-4">404</span>
+                                        </a>
+                                    </li>
+                                    <li className="relative px-6 py-3">
+                                        <a
+                                            className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                            href="blank.html"
+                                        >
+                                            <span className="ml-4">Blank</span>
+                                        </a>
+                                    </li>
+                                    <li className="relative px-6 py-3">
+                                        <a
+                                            className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                            href="profile.html"
+                                        >
+                                            <span className="ml-4">Profile</span>
+                                        </a>
+                                    </li>
+                                    <li className="relative px-6 py-3">
+                                        <a
+                                            className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                                            href="settings.html"
+                                        >
+                                            <span className="ml-4">Settings</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            )}
+                        </li>
+                    </ul>
+                </div>
             </aside>
             <div>
                 {children}
