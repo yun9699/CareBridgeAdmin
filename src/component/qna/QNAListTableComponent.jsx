dde965e8a171db655464d192c6912e3125054244
DSL0680 <ekthf988172@gmail.com>
2024-10-23 16:57:54 +0900

    feat : QNAListTableComponent - tr edit

--- src/component/qna/QNAListTableComponent.jsx
+++ src/component/qna/QNAListTableComponent.jsx
@@ -7,7 +7,7 @@ function QNAListTableComponent() {
     return (
         <table className="w-full whitespace-no-wrap">
             <thead>
-            <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+            <tr className="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b bg-gray-50">
                 <th className="px-4 py-3">Client</th>
                 <th className="px-4 py-3">Amount</th>
                 <th className="px-4 py-3">Status</th>
@@ -15,7 +15,7 @@ function QNAListTableComponent() {
                 <th className="px-4 py-3">Actions</th>
             </tr>
             </thead>
-            <tbody className="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+            <tbody className="bg-white divide-y">
             <tr className="text-gray-700 dark:text-gray-400">
                 <td className="px-4 py-3">
                     <div className="flex items-center text-sm">
