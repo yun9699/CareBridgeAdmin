172bf6694f0f089fc43a474cfa17b2c91c9a8cac
DSL0680 <ekthf988172@gmail.com>
2024-10-24 18:44:59 +0900

    feat : suspense / loading add

--- src/router/CareGiverRouter.jsx
+++ src/router/CareGiverRouter.jsx
@@ -1,15 +1,18 @@
-import {lazy} from "react";
+import {lazy, Suspense} from "react";
+import LoadingPage from "@/page/LoadingPage.jsx";
 
 const CareGiverIndex = lazy(() => import('../page/caregiver/CareGiverIndex.jsx'))
 const CareGiver = lazy(() => import('../page/caregiver/CareGiverPage'))
 
+const Loading = <LoadingPage/>
+
 const CareGiverRouter = {
     path: "/caregiver",
-    element: <CareGiverIndex/>,
+    element: <Suspense fallback={Loading}><CareGiverIndex/></Suspense>,
     children: [
         {
             path: "",
-            element: <CareGiver/>
+            element: <Suspense fallback={Loading}><CareGiver/></Suspense>
         }
     ]
 }
