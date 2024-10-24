051ba76b040b98ac48e359d7993f8a2b842b7340
DSL0680 <ekthf988172@gmail.com>
2024-10-24 18:44:09 +0900

    feat : layout sidebar Link add

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
+import {Link} from "react-router-dom";
 
-function Sidebar(props) {
+function Sidebar() {
 
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const [isPagesMenuOpen, setIsPagesMenuOpen] = useState(false);
@@ -11,19 +12,19 @@ function Sidebar(props) {
                 className={`z-20 ${sidebarOpen ? 'block' : 'hidden'} w-64 overflow-y-auto bg-white md:block flex-shrink-0`}
             >
                 <div className="py-4 text-gray-500">
-                    <a className="ml-6 text-lg font-bold text-gray-800 flex items-center" href="#">
+                    <Link className="ml-6 text-lg font-bold text-gray-800 flex items-center" to="/">
                         <img src="/logo.png" alt="Logo" className="mr-2 h-8"/>
                         CareBridge
-                    </a>
+                    </Link>
                     <ul className="mt-6">
                         <li className="relative px-6 py-3">
                             <span
                                 className="absolute inset-y-0 left-0 w-1 bg-green-600 rounded-tr-lg rounded-br-lg"
                                 aria-hidden="true"
                             ></span>
-                            <a
+                            <Link
                                 className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-green-600"
-                                href="index.html"
+                                to="/"
                             >
                                 <svg
                                     className="w-5 h-5"
@@ -40,12 +41,12 @@ function Sidebar(props) {
                                     ></path>
                                 </svg>
                                 <span className="ml-4">Dashboard</span>
-                            </a>
+                            </Link>
                         </li>
                         <li className="relative px-6 py-3">
-                            <a
+                            <Link
                                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-green-600"
-                                href="forms.html"
+                                to="/caregiver"
                             >
                                 <svg
                                     className="w-5 h-5"
@@ -61,13 +62,13 @@ function Sidebar(props) {
                                         d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
                                     ></path>
                                 </svg>
-                                <span className="ml-4">Forms</span>
-                            </a>
+                                <span className="ml-4">CareGiver</span>
+                            </Link>
                         </li>
                         <li className="relative px-6 py-3">
-                            <a
+                            <Link
                                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-green-600"
-                                href="cards.html"
+                                to="/careTaker/list"
                             >
                                 <svg
                                     className="w-5 h-5"
@@ -83,13 +84,13 @@ function Sidebar(props) {
                                         d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
                                     ></path>
                                 </svg>
-                                <span className="ml-4">Cards</span>
-                            </a>
+                                <span className="ml-4">CareTaker</span>
+                            </Link>
                         </li>
                         <li className="relative px-6 py-3">
-                            <a
+                            <Link
                                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-green-600"
-                                href="charts.html"
+                                to="/qna/list"
                             >
                                 <svg
                                     className="w-5 h-5"
@@ -106,13 +107,13 @@ function Sidebar(props) {
                                     ></path>
                                     <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
                                 </svg>
-                                <span className="ml-4">Charts</span>
-                            </a>
+                                <span className="ml-4">QNA</span>
+                            </Link>
                         </li>
                         <li className="relative px-6 py-3">
-                            <a
+                            <Link
                                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-green-600"
-                                href="buttons.html"
+                                to="/faq"
                             >
                                 <svg
                                     className="w-5 h-5"
@@ -128,50 +129,8 @@ function Sidebar(props) {
                                         d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
                                     ></path>
                                 </svg>
-                                <span className="ml-4">Buttons</span>
-                            </a>
-                        </li>
-                        <li className="relative px-6 py-3">
-                            <a
-                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-green-600"
-                                href="modals.html"
-                            >
-                                <svg
-                                    className="w-5 h-5"
-                                    aria-hidden="true"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                                    ></path>
-                                </svg>
-                                <span className="ml-4">Modals</span>
-                            </a>
-                        </li>
-                        <li className="relative px-6 py-3">
-                            <a
-                                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-green-600"
-                                href="tables.html"
-                            >
-                                <svg
-                                    className="w-5 h-5"
-                                    aria-hidden="true"
-                                    fill="none"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
-                                </svg>
-                                <span className="ml-4">Tables</span>
-                            </a>
+                                <span className="ml-4">FAQ</span>
+                            </Link>
                         </li>
                     </ul>
                     <ul className="mt-6">
