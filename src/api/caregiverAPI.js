cbd59f970712ed6a79e36dfdd2b03ecd483751fe
Dongeon Lee <duqdjs123@naver.com>
2024-10-28 09:24:50 +0900

    chore: host - edit

--- src/api/caregiverAPI.js
+++ src/api/caregiverAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const host = 'http://10.10.10.105:8080/api/v1/caregiver'
-const host = 'http://localhost:8080/api/v1/caregiver'
+const host = 'http://10.10.10.105:8080/api/v1/caregiver'
+// const host = 'http://localhost:8080/api/v1/caregiver'
 
 export const getCareGiverList = async (page) => {
 
