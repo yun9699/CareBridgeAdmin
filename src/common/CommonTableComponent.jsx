1f0ba4b5c5980dfa2f1269277481f64eca742306
jhyeong <hyeongcode@gmail.com>
2024-10-29 09:40:07 +0900

    clore: qna list red text

--- src/common/CommonTableComponent.jsx
+++ src/common/CommonTableComponent.jsx
@@ -149,9 +149,12 @@ function CommonTableComponent({ tableHeader, column, listFn, detailFn, delfn, up
                     <tr key={item[column[0]]} className="hover:bg-gray-100 border-b border-gray-200">
                         {/* 첫 번째 항목을 key로만 사용하고, 출력하지 않음 */}
                         {column.slice(1).map((temp) => (
-                            <td key={temp} className="px-5 py-4 text-sm text-gray-600" >
-                                {temp === 'checkAnswer' ? (item[temp] ? "답변완료" : "답변대기") :
-                                    temp.endsWith('Date') ? formatDate(item[temp]) : item[temp]}
+                            <td key={temp} className="px-5 py-4 text-sm text-gray-600">
+                                {temp === 'checkAnswer' ? (
+                                    <span className={item[temp] ? "text-gray-600" : "text-red-500"}>
+                                        {item[temp] ? "답변완료" : "답변대기"}
+                                    </span>
+                                ) : temp.endsWith('Date') ? formatDate(item[temp]) : item[temp]}
                             </td>
                         ))}
 
